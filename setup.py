6dee48409a95f50e0b5ce444b24af4e1ee0ffcee
YYY <1004473299@qq.com>
2019-10-18 13:38:16 +0800

    改名

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
+
+with open('LICENSE', encoding='utf-8') as f:
+    license = f.read()
 
 with open('requirements.txt', encoding='utf-8') as f:
-	reqs = f.read()
+    reqs = f.read()
+
+pkgs = [p for p in find_packages() if p.startswith('YTools')]
+print(pkgs)
 
 setup(
-	name = 'YYYTools',
-	version = '0.1',
-	description = '',
-	url = 'http://github.com/FFTYYY/YYYTools',
-	author = 'Yang Yongyi',
+    name='YTools',
+    version='0.1',
+    url='http://github.com/FFTYYY/YTools',
+    description='',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author = 'Yang Yongyi',
 	author_email = '1004473299@qq.com',
-	license = 'MIT',
-	packages = ['YYYTools'],
-	zip_safe = False,
+    python_requires = '>=3.6',
+    packages = pkgs,
     install_requires = reqs.strip().split('\n'),
 )
