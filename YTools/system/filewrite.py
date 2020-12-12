9dafdbb02790f1def4824e2d3eaa1200f0a186a2
YYY <1004473299@qq.com>
2020-12-13 07:39:57 +0800

    一个写文件的模块

---
+++ YTools/system/filewrite.py
@@ -0,0 +1,14 @@
+'''只要保证写的位置不重合，所有进程可以同时写文件！'''
+
+def get_filesize(file):
+	file.seek(0 , 2) #移到文件末尾
+	return file.tell()
+
+def write_file(file , pos , data):
+	file.seek(pos , 0) #从头偏移
+	return file.write(data)
+
+def read_file(file , pos , size):
+	'''从pos开始读len个字节'''
+	file.seek(pos , 0) #从头偏移
+	return file.read(size)
