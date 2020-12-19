b9f127d2efbb02c464ec2487f73f767f09bbd47b
YYY <1004473299@qq.com>
2020-12-19 16:54:28 +0800

    自动flush

--- YTools/system/filewrite.py
+++ YTools/system/filewrite.py
@@ -4,9 +4,12 @@ def get_filesize(file):
 	file.seek(0 , 2) #移到文件末尾
 	return file.tell()
 
-def write_file(file , pos , data):
+def write_file(file , pos , data , flush = True):
 	file.seek(pos , 0) #从头偏移
-	return file.write(data)
+	file.write(data)
+	if flush:
+		file.flush()
+	return file
 
 def read_file(file , pos , size):
 	'''从pos开始读len个字节'''
