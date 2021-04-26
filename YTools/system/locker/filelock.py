e30c04747a3a504e681feaefbbfcfb4906ad04ae
YYY <1004473299@qq.com>
2021-04-27 00:26:37 +0800

    写了一个只基于文件读写的锁服务
    TODO：用filelocker实现locker

---
+++ YTools/system/locker/filelock.py
@@ -0,0 +1,48 @@
+'''注意，这个模块可能死锁'''
+
+from ..fakepath import fakepath_abs , new_fakefolder
+import os
+import random
+import os.path as P
+
+my_id = "{pid}_{seed}".format( pid = os.getpid() , seed = random.randint(0,233333)) #进程相关的id
+name = "YFILELOCK/"
+new_fakefolder(name)
+
+def get_path(foldername):
+	'''给定lock的文件夹名，创建对应文件夹'''
+	pt = fakepath_abs(P.join(name , foldername))
+	new_fakefolder(pt)
+	return pt
+
+def checkfile(path , my_filename):
+	'''参数是绝对路径，检查这个路径下面有没有.lock结尾的文件，返回True表示没有。'''
+	return len( [ x for x in os.listdir(path) if x.endswith(".lock") and x != my_filename] ) == 0
+
+
+def acquire_lock(foldername , my_name = "0"):
+	my_filename = "{my_id}_{my_name}.lock".format(my_id = my_id , my_name = my_name) #生成自己的锁文件的文件名
+	path = get_path(foldername) #锁目录
+	my_file = P.join(path , my_filename) #自己的锁文件的文件路径
+
+	while True:	
+		# 等待其他进程释放锁
+		while not checkfile(path , my_filename): pass
+
+		# 上自己的锁
+		open(my_file , "w").close()
+
+		if not checkfile(path , my_filename): # 如果发现还有不是自己的锁
+			os.remove(my_file)  # 就删掉自己的锁
+			continue 			# 并重新检查
+
+		break # 加锁成功
+
+def release_lock(foldername , my_name = "0"):
+	my_filename = "{my_id}_{my_name}.lock".format(my_id = my_id , my_name = my_name) #生成自己的锁文件的文件名
+	path = get_path(foldername) #锁目录
+	my_file = P.join(path , my_filename) #自己的锁文件的文件名
+	os.remove(my_file)  # 删掉自己的锁
+
+
+
--- YTools/system/locker/lock.py
+++ YTools/system/locker/lock.py
@@ -4,91 +4,16 @@
 	安全性
 '''
 
-from ...network.communicate import SendServer , ListenServer , randport
-from ...network.protocol import bytes2str , str2bytes , bytes2int , int2bytes , bytes2ip , ip2bytes
 import time
 import sys
-from .lock_msg import Message
 import threading
 from subprocess import Popen
 
-def run_server(args = []):
-
-	Popen(args = [sys.executable , "-m" , "YTools_outer.system.locker.start_lock_server"] + args , shell = False)
-
-
 class LockerClient:
 
-	BAD_SIGNAL = "YTools_BAD" #表示没有收到信号
 
-	def __init__(self , server_ip = "127.0.0.1" , server_port = 34510 , listen_ip = "127.0.0.1" , patience = 2 , poolsize = 1024):
-		self.server_ip = server_ip
-		self.server_port = server_port
-
-		self.lis_ip   = listen_ip
-		self.lis_port = randport()
-		self.listener = ListenServer(host = self.lis_ip , port = self.lis_port , callback = self.lis_callback)
-		self.listener.start()
-
-		self.try_to_connect()
-
-		self.request_id = 0
-		self.response_pool  = [None for _ in range(poolsize)] 					#循环使用的多个消息池，防止顺序问题
-		self.seamahore_pool = [threading.Semaphore(0) for _ in range(poolsize)] #每个消息池收到消息，用信号量表示
-		self.patience = patience
-
-	def lis_callback(self , data , addr , who_get):
-		msg = Message(data = data)
-		self.response_pool[msg.id] = msg.value
-		self.seamahore_pool[msg.id].release()
-		
-	def try_to_connect(self):
-		'''连接或启动server'''
-		self.sender = SendServer()
-
-		flag = False
-		while not flag:
-			flag = self.sender.add_target(self.server_ip , self.server_port)
-			
-			if not flag:
-				run_server(args = [
-					"--ip={0}".format(self.server_ip) , 
-					"--port={0}".format(self.server_port) , 
-				])
-
-	def send_msg(self , msg):
-		return self.sender.send(msg.data)
-
-	def make_msg(self , type , key , val , id):
-		return Message(type , key , val , src_ip = self.lis_ip , src_port = self.lis_port , id = id)
-
-	def make_and_send(self , type , key , val , id):
-		return self.send_msg(self.make_msg(type , key , val , id = id))
-
-	def wait_return_val(self , func , *args , **kwargs):
-		# 调用函数
-
-		my_id = self.request_id #当前消息的id，期望回复也使用这个id（注意这个是单线程的所以没关系）
-		self.request_id = (self.request_id + 1) % len(self.response_pool) #循环使用消息池
-		self.response_pool[my_id] = self.BAD_SIGNAL #表示没有收到回复
-
-		bad_sender = func(*args , **kwargs , id = my_id)
-		while len(bad_sender) > 0:
-			print("连接失败 : {0} , {1}".format(args , kwargs))
-			self.try_to_connect()
-			bad_sender = func(*args , **kwargs , id = my_id)
-
-		# 等待返回值
-		self.seamahore_pool[my_id].acquire(timeout = self.patience) #等待信号量
-
-		while self.response_pool[my_id] is self.BAD_SIGNAL: 	#如果实际上没有得到值（信号量超时）
-			print ("未收到回复 : {0} , {1}".format(args , kwargs))
-			self.try_to_connect() 					#重新建立连接
-			func(*args , **kwargs , id = my_id) 	#重新发送
-			self.seamahore_pool[my_id].acquire(timeout = self.patience) #重新等待信号量
-
-		return self.response_pool[my_id]
-
+	def __init__(self):
+		pass
 	def get(self , key):
 		return self.wait_return_val(self.make_and_send , type = "ask" , key = key , val = None)
 
